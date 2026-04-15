958b33284ce055f0657272d7d8b3e44f9d1972eb
pabloflores465 <pablopolis2016@gmail.com>
2026-04-14 23:26:43 -0600

    Use nixosConfigurations for ISO build

--- flake.nix
+++ flake.nix
@@ -6,59 +6,57 @@
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    
-    configuration = { config, pkgs, lib, ... }: {
-      imports = [
-        "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-        "${nixpkgs}/nixos/modules/profiles/installation-device.nix"
-      ];
-      
-      # Boot
-      boot.loader.grub.enable = true;
-      boot.loader.grub.device = lib.mkDefault "/dev/sda";
-      boot.supportedFilesystems = [ "ext4" "vfat" ];
-      
-      # Enable flakes
-      nix.settings.experimental-features = [ "nix-command" "flakes" ];
-      
-      # Locale
-      i18n.defaultLocale = "en_US.UTF-8";
-      time.timeZone = lib.mkDefault "UTC";
-      
-      # Mesh networking
-      boot.kernelModules = [ "batman-adv" "mac80211" "cfg80211" ];
-      
-      # Network
-      networking.firewall.enable = false;
-      
-      # Packages
-      environment.systemPackages = with pkgs; [
-        batctl iw iproute2 ffmpeg mplayer mpg123
-        python3 curl wget git vim htop
-      ];
-      
-      # Services
-      services.openssh.enable = true;
-      
-      # Users
-      users.users.mesh = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        initialPassword = "mesh123";
-      };
-      users.users.root.initialPassword = "root123";
-      
-      # Sound
-      sound.enable = true;
-    };
   in
   {
-    packages.${system}.default = 
-      (nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ configuration ];
-      }).config.system.build.isoImage;
+    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+        "${nixpkgs}/nixos/modules/profiles/installation-device.nix"
+        
+        {
+          # Boot
+          boot.loader.grub.enable = true;
+          boot.loader.grub.device = "/dev/sda";
+          boot.supportedFilesystems = [ "ext4" "vfat" ];
+          
+          # Enable flakes
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          
+          # Locale
+          i18n.defaultLocale = "en_US.UTF-8";
+          time.timeZone = "UTC";
+          
+          # Mesh networking
+          boot.kernelModules = [ "batman-adv" "mac80211" "cfg80211" ];
+          
+          # Network
+          networking.firewall.enable = false;
+          
+          # Packages
+          environment.systemPackages = with import nixpkgs { inherit system; }; [
+            batctl iw iproute2 ffmpeg mplayer mpg123
+            python3 curl wget git vim htop
+          ];
+          
+          # Services
+          services.openssh.enable = true;
+          
+          # Users
+          users.users.mesh = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+            initialPassword = "mesh123";
+          };
+          users.users.root.initialPassword = "root123";
+          
+          # Sound
+          sound.enable = true;
+        }
+      ];
+    };
     
+    packages.${system}.default = self.nixosConfigurations.iso.config.system.build.isoImage;
     defaultPackage.${system} = self.packages.${system}.default;
   };
 }
