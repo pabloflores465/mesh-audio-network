72d3bf664b7a1fb47626155a6178d9ddb77c8697
pabloflores465 <pablopolis2016@gmail.com>
2026-04-14 23:19:04 -0600

    Fix flake.nix syntax

--- flake.nix
+++ flake.nix
@@ -12,34 +12,26 @@
         "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
       ];
       
-      # Boot
       boot.loader.grub.enable = true;
       boot.loader.grub.device = "/dev/sda";
       boot.supportedFilesystems = [ "ext4" "vfat" ];
       
-      # Enable flakes
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
       
-      # Locale
       i18n.defaultLocale = "en_US.UTF-8";
       time.timeZone = "UTC";
       
-      # Mesh networking
       boot.kernelModules = [ "batman-adv" "mac80211" "cfg80211" ];
       
-      # Network
       networking.firewall.enable = false;
       
-      # Packages
       environment.systemPackages = with pkgs; [
         batctl iw iproute2 ffmpeg mplayer mpg123
         python3 curl wget git vim htop
       ];
       
-      # Services
       services.openssh.enable = true;
       
-      # Users
       users.users.mesh = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
@@ -47,18 +39,17 @@
       };
       users.users.root.initialPassword = "root123";
       
-      # Sound
       sound.enable = true;
     };
-  in {
-    packages.${system}.default = (import nixpkgs {
+    
+    isoSystem = (import nixpkgs {
       inherit system;
     }).nixosSystem {
       inherit configuration;
-    }).config.system.build.isoImage;
-    
-    packages.${system}.iso = self.packages.${system}.default;
-    
-    defaultPackage.${system} = self.packages.${system}.default;
+    };
+  in
+  {
+    packages.${system}.default = isoSystem.config.system.build.isoImage;
+    defaultPackage.${system} = isoSystem.config.system.build.isoImage;
   };
 }
